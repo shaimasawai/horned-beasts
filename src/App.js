c16f27a616973b09b310421d65b257d499ebf10e
shaimasawai <shaima.alsawaie@gmail.com>
2021-09-07 09:41:28 -0400

    finle i solved

--- src/App.js
+++ src/App.js
@@ -2,36 +2,36 @@ import React,{Component } from "react";
 import Header from './componente/Header';
 import Main from './componente/Main';
 import Footer from './componente/Footer';
-import data from "./componente/data.json"
+// import data from "./componente/data.json"
 
 class App extends Component {
-  constructor(props){
-    super(props)
-    this.state={
-      data:data,
-        show:false,
-        dataa:{},
-  }
-}
-showdata = (data)=>{
-  this.setState({
-    show:true,
-    dataa: data,
+//   constructor(props){
+//     super(props)
+//     this.state={
+//       data:data,
+//         show:false,
+//         dataa:{},
+//   }
+// }
+// showdata = (data)=>{
+//   this.setState({
+//     show:true,
+//     dataa: data,
 
 
-  })
-}
-close = () =>{
-  this.setState({
-    show:false,
-})
-}
+//   })
+// }
+// close = () =>{
+//   this.setState({
+//     show:false,
+// })
+// }
   render(){
    
     return(
       <div>
         <Header/>
-        <Main  selectedmodal ={this.showdata} data={this.state.data} />
+        <Main   />
       
        
         <Footer/>
--- src/componente/Main.js
+++ src/componente/Main.js
@@ -1,32 +1,33 @@
 import React, { Component } from 'react'
-import HomedBeast from './HomedBeast'
-import data from './data.json'
+import hornedbeastsdata from './data.json'
+import HoredBeast from './HoredBeast'
+
 
 
 
 
 class Main extends Component {
     render() {
-       
+
         return (
-            <div>
-            {
-                this.props.data.map(item=>{
-                    return(
-                        <HomedBeast
-                        title ={item.title}
-                        img_url={item.image_url}
-                        description ={item.description}
-                        mofal ={this.props.selectedmodal}
-                        horns={this.props.horns}
-                    />
-                    )
-                })
-            }
+            <>
+                {
+                    hornedbeastsdata.map(item => {
+                        return (
+                            
+                            <HoredBeast
+                                title={item.title}
+                                image_url={item.image_url}
+                                description={item.description}
+
+                            />
+                        )
+                    })
+                }
 
 
 
-            </div>
+            </>
         )
     }
 }
