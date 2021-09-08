5a9fa45f303a0d001eecd5edc00f7912b0c189df
shaimasawai <shaima.alsawaie@gmail.com>
2021-09-08 12:09:57 -0400

    fixed every thing

--- src/App.js
+++ src/App.js
@@ -1,52 +1,47 @@
-import React,{Component } from "react";
-import Header from './componente/Header';
-import Main from './componente/Main';
-import Footer from './componente/Footer';
-import Databeast from './componente/data.json'
-import Selectbast  from "./componente/selectbast";
-// import 'bootstrap/dist/css/bootstrap.min.css';
-
+import React, { Component } from "react";
+import Header from "./componente/Header";
+import Main from "./componente/Main";
+import Footer from "./componente/Footer";
+import Databeast from "./componente/data.json";
+import Selectbast from "./componente/selectbast";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
-  constructor(props){
-    super(props)
-    this.state={
-      dataHorne:Databeast,
-        show:false,
-        datamodle:{},
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataHorne: Databeast,
+      show: false,
+      datamodle: {},
+    };
   }
-}
-handleShow = (data)=>{
-  this.setState({
-    show:true,
-    datamodle: data,
+  handleShow = (data) => {
+    this.setState({
+      show: true,
+      datamodle: data,
+    });
+  };
+  handleClose = () => {
+    this.setState({
+      show: false,
+    });
+  };
 
+  render() {
+    return (
+      <>
+        <Header />
+        <Main chooesmodle={this.handleShow} data={this.dataHorne} />
+        <Selectbast
+          exit={this.handleClose}
+          showmodle={this.state.show}
+          datamodle={this.state.datamodle}
+        />
 
-  })
-}
-handleClose = () =>{
-  this.setState({
-    show:false,
-})
-}
-
-
-
-  render(){
-   
-    return(
-      <div>
-        <Header/>
-        <Main chooesmodle={this.handleShow} data={this.dataHorne}  />
-        <Selectbast exit ={this.handleClose} showmodle={this.state.show}  datamodle= {this.props.datamodle} />
-        
-      
-       
-        <Footer/>
-
-      </div>
-    )
+        <Footer />
+      </>
+    );
   }
 }
 
-export default App
+export default App;
--- src/componente/HoredBeast.js
+++ src/componente/HoredBeast.js
@@ -1,73 +1,65 @@
-import React, { Component } from 'react'
-import {Card,Button} from 'react-bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from "react";
+import { Card, Button, Col } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
- class HoredBeast extends Component {
-     constructor(props){
-         super(props)
-         this.state={
-            
-             likes :0,
-             vote:0,
+class HoredBeast extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      likes: 0,
+      vote: 0,
+    };
+  }
+  //  likesandimg =()=>{
+  //      this.setState(
+  //          {
+  //              NUMBERIMG:this.state.NUMBERIMG+1,
+  //              likes: this.state.likes+1,
+  //          }
+  //      )
+  //  }
 
-         }
-     }
-    //  likesandimg =()=>{
-    //      this.setState(
-    //          {
-    //              NUMBERIMG:this.state.NUMBERIMG+1,
-    //              likes: this.state.likes+1,
-    //          }
-    //      )
-    //  }
+  clickimg = () => {
+    this.setState({
+      likes: this.state.likes + 1,
+      vote: this.state.vote + 1,
+    });
+  };
+  clickpuchtr = () => {
+    this.props.modle({
+      title: this.props.title,
+      image_url: this.props.image_url,
+      description: this.props.description,
+    });
+  };
 
-     clickimg=() =>{
-         this.setState({
-            likes:this.state.likes+1,
-            vote:this.state.vote+1,
-         })
-         
-         
-     }
-     clickpuchtr =()=>{
-         this.props.modle(
-             {
-                title: this.props.title,
-                image_url: this.props.image_url,
-                description: this.props.description
-             }
-         )
-     }
-
-
-    render() {
-        return (
-            <div>
-              {/* <h2> {this.props.title}</h2>
-              <img style={{width:"600px"}} onClick={this.clickimg} src={this.props.image_url} title={this.props.title} alt={this.props.title} />
-              <h3>{this.state.likes} 💖 </h3>
-              <h4>{this.props.description}</h4>
-              <h4>{this.props.keyword}</h4>
-              <h4>{this.props.horns}</h4> */}
-
-    <Card style={{ width: "60%", height: "80%"  }} bg={'warning'} >
-                      <Card.Img width = {300} height= {400} onClick={this.clickimg} variant="top" src={this.props.image_url} alt={this.props.title}/>
-                      <Card.Body>
-                          <Card.Title > {this.props.title}</Card.Title>
-                          <Card.Text>{this.props.description}</Card.Text>
-                      </Card.Body>
-                      <Button variant="info">💖 {this.state.vote}</Button>
-                      <Button onClick={this.clickpuchtr} variant="danger">  click Me</Button>
-
-                      </Card>
-
-              
-
-
-            </div>
-        )
-    }
+  render() {
+    return (
+      <>
+        <Col xs={4}>
+          <Card style={{ width: "60%", height: "80%" }} bg={"warning"}>
+            <Card.Img
+              width={300}
+              height={400}
+              onClick={this.clickimg}
+              variant="top"
+              src={this.props.image_url}
+              alt={this.props.title}
+            />
+            <Card.Body>
+              <Card.Title> {this.props.title}</Card.Title>
+              <Card.Text>{this.props.description}</Card.Text>
+            </Card.Body>
+            <Button variant="info">💖 {this.state.vote}</Button>
+            <Button onClick={this.clickpuchtr} variant="danger">
+              {" "}
+              click Me
+            </Button>
+          </Card>
+        </Col>
+      </>
+    );
+  }
 }
 
-export default HoredBeast
-
+export default HoredBeast;
